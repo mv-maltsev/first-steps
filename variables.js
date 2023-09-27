1088fff6ee63355df70613f62c96da8c68ade10d
mv-maltsev <mv.maltsev@gmail.com>
2023-09-27 10:59:40 +0300

    first

---
+++ variables.js
@@ -0,0 +1,14 @@
+
+// Работа с переменными
+let admin;
+let name = "Джон";
+admin = name;
+console.log(admin);
+
+// Придумайте правильные имена
+let nameOurPlanet;
+let currentUser;
+
+// Какие буквы (заглавные или строчные) использовать для имён констант?
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
